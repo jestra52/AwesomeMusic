d3bd50caae9a749935aecaee776b22e2cc1ba634
Juan Carlos Estrada Alvarez <jestra52@hotmail.com>
2022-03-08 02:31:10 -0500

    Removed hardcoded keys and connection strings

--- AwesomeMusic/AwesomeMusic/Program.cs
+++ AwesomeMusic/AwesomeMusic/Program.cs
@@ -19,28 +19,19 @@ namespace AwesomeMusic
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((context, config) =>
                 {
-                    // First, I used a keyvault to safely access some items such as connection strings,
-                    // but I realized that it was kinda difficult to test locally for other users,
-                    // because I needed to add them first into the Active Directory in Azure, as well
-                    // create some access policies.
-                    //
-                    // So, in order to make it easier to test locally, I just added the keys from the keyvault
-                    // directly into the appsettings.json file. I know this is not the most safe approach, but
-                    // for now and also for the purpose of this test, it can be ignored.
+                    var settings = config.Build();
+                    var vaultName = $"https://{settings["AwmVaultName"]}.vault.azure.net/";
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(async (auth, resource, scope) =>
+                    {
+                        var credentials = new DefaultAzureCredential(false);
+                        var token = await credentials.GetTokenAsync(
+                            new Azure.Core.TokenRequestContext(new[] { "https://vault.azure.net/.default" }));
+                        return token.Token;
+                    });
 
-                    //var settings = config.Build();
-                    //var vaultName = $"https://{settings["AwmVaultName"]}.vault.azure.net/";
-                    //var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                    //var keyVaultClient = new KeyVaultClient(async (auth, resource, scope) =>
-                    //{
-                    //    var credentials = new DefaultAzureCredential(false);
-                    //    var token = await credentials.GetTokenAsync(
-                    //        new Azure.Core.TokenRequestContext(new[] { "https://vault.azure.net/.default" }));
-                    //    return token.Token;
-                    //});
-
-                    //config.AddAzureKeyVault(
-                    //   vaultName, keyVaultClient, new DefaultKeyVaultSecretManager());
+                    config.AddAzureKeyVault(
+                       vaultName, keyVaultClient, new DefaultKeyVaultSecretManager());
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
